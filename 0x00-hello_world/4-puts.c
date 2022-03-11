8b8a7f04119b54d91dca2f41e720102d2cfa9bdc
theodoregit <tedoteams@gmail.com>
2022-03-11 13:06:00 -0800

    putssss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /*
  * Main Entry point
  * printing a string
- * */
+ */
 int main(void)
 {
 	char pro[] = "\"Programming is like building a multilingual puzzle";
 	puts(pro);
-	return (0);		
+	return(0);		
 }
