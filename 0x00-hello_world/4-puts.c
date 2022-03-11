4c6a23614a3b82052ab58bdcb5e67734f0c05487
theodoregit <tedoteams@gmail.com>
2022-03-11 12:48:54 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	    //string initialisation
-	    char Mystr[] = "\"Programming is like building a multilingual puzzle";
-	    //         
-	    puts(Mystr); //writing the string to stdout
-	    //                  
+	    char Mystr[] = "\"Programming is like building a multilingual puzzle";         
+	    puts(Mystr);
 	    return 0;
 
