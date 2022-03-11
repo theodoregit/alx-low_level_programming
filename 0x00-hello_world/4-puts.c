b7c53b01611f79170717ef3d3bbf8db4129a1e25
theodoregit <tedoteams@gmail.com>
2022-03-11 12:38:29 -0800

    puttingstring

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-	puts("Programming is like building a multilingual puzzle");
+	char pro[] = "Programming is like building a multilingual puzzle";
+	puts(pro);
 	return 0;
 }
