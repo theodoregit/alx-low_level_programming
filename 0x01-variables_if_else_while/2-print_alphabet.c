54272dfd3f9be7a3ee7c7ebcb806fe82ad000225
theodoregit <tedoteams@gmail.com>
2022-03-13 11:05:09 -0700

    putingchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet
+ * Description:
+ * Return: 0 and exit
+ */
+int main () {
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+
+	}
+	putchar('\n');
+        return (0);
+}
