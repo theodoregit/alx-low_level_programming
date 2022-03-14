d31ab12bc33e2dc964746d7a74261831aa24d44c
theodoregit <tedoteams@gmail.com>
2022-03-14 14:17:09 -0700

    twodigitscombination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,11 +10,10 @@ int main(void)
 
 	for (d1 = 0; d1 < 9; d1++)
 	{
-		for (d2 = d1 +1; d2 < 10; d2++)
+		for (d2 = d1 + 1; d2 < 10; d2++)
 		{
 			putchar((d1 % 10) + '0');
 			putchar((d2 % 10) + '0');
-		
 			if (d1 == 8 && d2 == 9)
 				continue;
 
