f32bb71320fb56801f0b0dedae05a00bcebd5929
theodoregit <tedoteams@gmail.com>
2022-03-13 22:39:54 -0700

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet
+ * Description:
+ * Return: 0 and exit
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = '0'; alpha <= '9'; alpha++)
+	{
+		putchar(alpha);
+	}
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
