72d189ff46c4afa62aae88f5c3d068e3f99c264c
theodoregit <tedoteams@gmail.com>
2022-03-14 12:25:11 -0700

    lastone

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * Description: desc
  * Return: 1 std error
  */
-int main (void)
+int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
