a0848ba9454a0a73acdd1ce4278c6e89c2c0e5fb
theodoregit <tedoteams@gmail.com>
2022-03-14 14:07:17 -0700

    comb1

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,12 @@ int main(void)
 {
 	int alpha;
 
-	for (alpha = '0'; alpha <= '9'; alpha++)
+	for (alpha = 0; alpha <= 9; alpha++)
 	{
 		putchar((alpha % 10) + '0');
-		if(alpha == 9)
+		if (alpha == 9)
 			continue;
+
 		putchar(',');
 		putchar(' ');
 	}
