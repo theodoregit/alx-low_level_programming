0ec714ffaff9a1fa6a388f88ebffe52b15778c5c
theodoregit <tedoteams@gmail.com>
2022-03-14 13:57:25 -0700

    doneeeee

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include<unistd.h>
 /**
- * main - std error
- * Description: desc
- * Return: 1 std error
+ * main - some desc
+ * Description: some desc
+ * Return: stderr 1
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-	fwrite(str, 1, sizeof(str), stderr);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
