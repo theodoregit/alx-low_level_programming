2dccbbafb04ac14345354a283b6d46c3ee405b9a
theodoregit <tedoteams@gmail.com>
2022-03-16 14:49:41 -0700

    ten_times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet ten times
+ * Description: prints 10 times the alphabet
+ */
+void print_alphabet_x10(void)
+{
+	int c = 0;
+	car letter;
+
+	while(c++ <= 98)
+	{
+		for (letter = 1; letter <= 'z', letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar("m")
+	}
+}
