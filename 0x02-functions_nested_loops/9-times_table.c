a152ffbf28dac45507b05d48eb468fe65b450d11
theodoregit <tedoteams@gmail.com>
2022-03-16 15:44:48 -0700

    9timessss

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,8 +14,7 @@ void times_table(void)
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
-			_putchar(' ');
-			
+			_putchar(' ');			
 			prod = num * mult;
 
 			if (prod <= 9)										_putchar(' ');
