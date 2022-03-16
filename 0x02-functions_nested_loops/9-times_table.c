ac76d7d09e6d9c550192481e10a5b3baf5a53281
theodoregit <tedoteams@gmail.com>
2022-03-16 14:34:41 -0700

    ninetimes

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,7 +5,7 @@
 void times_table(void)
 {
 	int num, mult, prod;
-	
+
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
@@ -15,7 +15,7 @@ void times_table(void)
 			_putchar(' ');
 			
 			prod = num * mult;
-			
+
 			if (prod <= 9)										_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
