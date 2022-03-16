24c4d5942cbbae0604fd1d251640768680efe115
theodoregit <tedoteams@gmail.com>
2022-03-16 15:22:11 -0700

    absoluting

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - return absolute value
+ * @n: any integer
+ * Return: the absolute value of an n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
