2935058aaa2f73cddf2a7aca202c15411edc18bd
theodoregit <tedoteams@gmail.com>
2022-03-18 00:22:25 -0700

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,7 +9,7 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-
+	
 	if (a > b && a > c)
 	{
 		largest = a;
@@ -22,9 +22,21 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
+	else if (a == b && a > c)
+	{
+		largest = a;
+	}
+	else if (a == c && a > b)
+	{
+		largest = a;
+	}
+	else if (b == c && b > a)
+	{
+		largest = b;
+	}
 	else
 	{
-		largest = c;
+		largest = a;
 	}
 
 	return (largest);
