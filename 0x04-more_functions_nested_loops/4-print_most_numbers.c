ea3186ffa3c6e9cd1418e453749b920c8a629dc6
theodoregit <tedoteams@gmail.com>
2022-03-19 00:11:06 -0700

    mostnumber

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the most numbers from 0 to 9
+ *
+ * Return: 0-9, except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if ((I ==2) || (i == 4))
+			continue;
+		else
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
