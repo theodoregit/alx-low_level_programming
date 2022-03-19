b37b3cee6430842b1d80ea5a03054235515bfce4
theodoregit <tedoteams@gmail.com>
2022-03-19 00:16:39 -0700

    printingmorenumbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main"
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ *
+ * Return: x10 numbers (0-14)
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
