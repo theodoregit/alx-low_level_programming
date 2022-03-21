6861f998227fb3e2e1e2e279d66b782ca51511a8
theodoregit <tedoteams@gmail.com>
2022-03-21 12:48:00 -0700

    reverse_print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - print string in reverse order
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int l = 0, i;
+	
+	while (s[i++])
+		l++;
+
+	for (i = l - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
