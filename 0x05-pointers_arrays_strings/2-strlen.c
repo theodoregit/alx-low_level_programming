11cafa025b44fdc23ccaf423caf1f78dcbed9db1
theodoregit <tedoteams@gmail.com>
2022-03-21 12:34:17 -0700

    length_of_string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - a fuction that returns the length of a given string
+ * @str: string
+ * Return: length of a tring
+ */
+size_t _strlen(const char *str)
+{
+	size_t l = 0;
+	
+	while (*str++)
+		l++;
+
+	return l;
+}
