9e10aa6cc72a94cefbfd649339622478b92754f6
theodoregit <tedoteams@gmail.com>
2022-03-21 13:35:30 -0700

    password_generator

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -30,23 +30,22 @@ int main(void)
 		if ((sum - 2772) % 2 != 0)
 			diff_half1++;
 
-			for (index = 0; password[index]; index++)
-
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1))
 			{
-				if (password[index] >= (33 + diff_half1))
-				{
-					password[index] -= diff_half1;
-					break;
-				}
+				password[index] -= diff_half1;
+				break;
 			}
-			for (index = 0; password[index]; index++)
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
 			{
-				if (password[index] >= (33 + diff_half2))
-				{
-					password[index] -= diff_half2;
-					break;
-				}
+				password[index] -= diff_half2;
+				break;
 			}
+		}
 	}
 
 	printf("%s", password);
