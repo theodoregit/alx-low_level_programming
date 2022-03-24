2d60c91b030cfeb0a2dce7d9ea0d3cb44d6dfc77
theodoregit <tedoteams@gmail.com>
2022-03-24 13:00:29 -0700

    some_fixes_on_spaces

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strncat - Concatenates two strings using at most
  *           an inputted number of bytes from src.
@@ -16,10 +15,8 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[index++])
 		dest_len++;
 
-
 	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
-
 
 	return (dest);
 }
